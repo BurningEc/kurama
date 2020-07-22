ce86fae243d4d2a742f43fe4c2135355be4731dc
proyectoA <davidvargas050695@gmail.com>
2020-07-22 13:20:47 -0500

    second commit

--- Sistema/bd/db.php
+++ Sistema/bd/db.php
@@ -1,7 +1,7 @@
 ﻿<?php
 /*Datos de conexion a la base de datos*/
-define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
-define('DB_USER', 'root');//Usuario de tu base de datos
-define('DB_PASS', '');//Contraseña del usuario de la base de datos
+define('DB_HOST', 'kurama3.cut2azi2qhcc.us-east-2.rds.amazonaws.com');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'kuramaadmin');//Usuario de tu base de datos
+define('DB_PASS', 'root1234');//Contraseña del usuario de la base de datos
 define('DB_NAME', 'kurama3');//Nombre de la base de datos
 ?>
