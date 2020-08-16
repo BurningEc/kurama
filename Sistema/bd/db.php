255e0331a641addbe5d1e3041c7edaeaeae721a3
proyectoA <davidvargas050695@gmail.com>
2020-08-16 14:51:09 -0500

    respaldo

--- Sistema/bd/db.php
+++ Sistema/bd/db.php
@@ -3,5 +3,5 @@
 define('DB_HOST', 'database-tienda1.cvhbx6auu2p6.us-east-1.rds.amazonaws.com');//DB_HOST:  generalmente suele ser "127.0.0.1"
 define('DB_USER', 'tiendaadmin');//Usuario de tu base de datos
 define('DB_PASS', 'root1234');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'kurama3');//Nombre de la base de datos
+define('DB_NAME', 'kurama');//Nombre de la base de datos
 ?>
